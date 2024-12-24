6d235009ef2eda79968f2430b2c581f1843d6a29
Felyks <gamingfelix78@gmail.com>
2024-12-24 21:37:17 +0700

    first commit
    react vite as base project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;
